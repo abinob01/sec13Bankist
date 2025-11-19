dc67f12b4b9f5dba446b78f203a210bdf0033b8d
abinob01 <abinob70@gmail.com>
2025-11-18 19:35:17 -0800

    collecting the images to one folder and handling smooh scrolling on the nav elements

--- script.js
+++ script.js
@@ -59,7 +59,8 @@ learnMore.addEventListener('click', function (e) {
 document.querySelector('.nav--links').addEventListener('click', function (e) {
   e.preventDefault();
 
-  if (e.target.classList.contains('.nav__link')) {
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
